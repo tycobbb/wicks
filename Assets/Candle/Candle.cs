a8dbb842bf3cfc3dde10d6768f426ac724de9b9c
Ty Cobb <ty.cobb.m@gmail.com>
2021-09-17 10:25:04 -0400

    add candle

---
+++ Assets/Candle/Candle.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Candle: MonoBehaviour {
+    // -- nodes --
+    /// the candle's point light
+    Transform mFlame;
+
+    /// the candle's flame
+    Transform mFlameEffect;
+
+    // -- lifecycle --
+    void Awake() {
+        var t = transform;
+
+        // capture node dependencies
+        mFlame = t.Find("Flame");
+        mFlameEffect = t.Find("Flame/Effect");
+    }
+
+    void Update() {
+        // rotate the flame
+        if (mFlame) {
+            RotateFlame();
+        }
+    }
+
+    // -- tools --
+    /// if the candle is lit or not (hot prop)
+    public bool IsLit {
+        get => mFlame.IsActive();
+        set => mFlame.SetActive(value);
+    }
+
+    // -- commands --
+    /// ensures the flame is upright
+    void RotateFlame() {
+        mFlameEffect.forward = Vector3.up;
+    }
+}

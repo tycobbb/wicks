11f88d51232a2c67d9f5864b5926ec88521c9344
Ty Cobb <ty.cobb.m@gmail.com>
2021-09-18 18:25:09 -0400

    fix hold & add rotation

--- Assets/House/Box.cs
+++ Assets/House/Box.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 /// a box for spawning candles
@@ -8,6 +9,9 @@ public class Box: MonoBehaviour {
 
     /// the number of candles to create
     [SerializeField] int mCount = 420;
+
+    /// the number of candles to spawn per frame
+    [SerializeField] int mSpawnRate = 5;
 
     // -- nodes --
     /// the root transform
@@ -20,12 +24,23 @@ public class Box: MonoBehaviour {
     }
 
     void Start() {
+        // spawn a bunch of candles
+        StartCoroutine(SpawnCandles());
+    }
+
+    // -- commands --
+    /// spawn candles over a few frames
+    IEnumerator SpawnCandles() {
         // make a bunch of candles
-        for (var i = 0; i < mCount; i++) {
+        for (var i = 1; i <= mCount; i++) {
             var obj = Instantiate(mCandle,  mRoot);
             obj.name = $"Candle{i}";
             obj.transform.localPosition = Vector3.zero;
             obj.GetComponent<Candle>().IsLit = false;
+
+            if (i % mSpawnRate == 0) {
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,12 +1,14 @@
-using System;
 using Hertzole.GoldPlayer;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player: MonoBehaviour {
     // -- constants --
-    /// a mask for the item layer
-    static int sItemMask = -1;
+    /// the item layer
+    static int sItemLayer = -1;
+
+    /// the held item layer
+    static int sHeldItemLayer = -1;
 
     // -- tuning --
     /// the maximum grab range
@@ -15,38 +17,65 @@ public class Player: MonoBehaviour {
     /// the radius of the grab sphere
     [SerializeField] float mGrabSlack = 0.5f;
 
-    // -- nodes --
-    /// the grab action
-    InputAction mGrab;
+    /// the speed the held item rotates every frame
+    [SerializeField] float mGrabRotateSpeed = 4.0f;
 
+    /// the maximum rotation for the held item
+    [SerializeField] float mGrabMaxRotation = 95.0f;
+
+    // -- nodes --
     /// the player's viewpoint
     Transform mView;
 
     /// the player's hand
-    FixedJoint mHand;
+    Transform mHand;
+
+    // -- inputs --
+    /// all the player's inputs
+    InputActionAsset mInputs;
+
+    /// the grab input
+    InputAction mGrab;
+
+    /// the grab rotate left input
+    InputAction mGrabRotateLeft;
+
+    /// the grab rotate right input
+    InputAction mGrabRotateRight;
 
     // -- props --
+    /// the held item, if any
+    Rigidbody mHeld;
+
+    /// the angle of the held item
+    float mHeldAngle = 0.0f;
+
     /// a shared buffer for raycast hits
     readonly RaycastHit[] mHits = new RaycastHit[1];
 
     // -- lifecycle --
     void Awake() {
-        var t = transform;
-        var inputs = GetComponent<GoldPlayerInputSystem>().InputAsset;
-
         // get node dependencies
-        mGrab = inputs.FindAction("grab");
+        var t = transform;
         mView = t.Find("Head/View");
-        mHand = t.Find("Hand").GetComponent<FixedJoint>();
+        mHand = t.Find("Hand");
+
+        // bind inputs
+        var inputs = GetComponent<GoldPlayerInputSystem>().InputAsset;
+        mInputs = inputs;
+        mGrab = inputs.FindAction("Grab");
+        mGrabRotateLeft = inputs.FindAction("Grab-RotateLeft");
+        mGrabRotateRight = inputs.FindAction("Grab-RotateRight");
 
         // set statics
-        if (sItemMask == -1) {
-            sItemMask = LayerMask.GetMask("Item");
+        if (sItemLayer == -1) {
+            sItemLayer = LayerMask.NameToLayer("Item");
+            sHeldItemLayer = LayerMask.NameToLayer("HeldItem");
         }
     }
 
     void OnEnable() {
-        mGrab.Enable();
+        mInputs.Enable();
     }
 
     void Update() {
@@ -54,13 +83,29 @@ public class Player: MonoBehaviour {
             TryGrabItem();
         }
 
-        if (IsHoldingItem && mGrab.WasReleasedThisFrame()) {
-            ReleaseItem();
+        if (mHeld != null) {
+            if (mGrabRotateLeft.IsPressed()) {
+                RotateItem(isLeft: true);
+            }
+
+            if (mGrabRotateRight.IsPressed()) {
+                RotateItem(isLeft: false);
+            }
+
+            if (mGrab.WasReleasedThisFrame()) {
+                ReleaseItem();
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        if (mHeld != null) {
+            HoldItem();
         }
     }
 
     void OnDisable() {
-        mGrab.Enable();
+        mInputs.Disable();
     }
 
     // -- commands--
@@ -72,7 +117,7 @@ public class Player: MonoBehaviour {
             mView.forward,
             mHits,
             mGrabRange,
-            sItemMask
+            1 << sItemLayer
         );
 
         if (nHits <= 0) {
@@ -87,30 +132,52 @@ public class Player: MonoBehaviour {
 
     /// grabs the item
     void GrabItem(Rigidbody item) {
-        // move the item into the hand
-        var ti = item.transform;
-        ti.up = Vector3.up;
-        ti.position = mHand.transform.position;
+        mHeld = item;
+        mHeldAngle = 0.0f;
 
-        // clear the item's physics state
+        // disable physics while holding the item
+        item.isKinematic = true;
+
+        // disable collisions w/ player
+        item.gameObject.layer = sHeldItemLayer;
+
+        // reorient it
+        item.transform.up = Vector3.up;
+
+        // clear its physics state
         item.velocity = Vector3.zero;
         item.angularVelocity = Vector3.zero;
+    }
 
-        // weld it to the hand
-        mHand.SetActive(true);
-        mHand.connectedBody = item;
+    /// rotate the item
+    void RotateItem(bool isLeft) {
+        mHeldAngle += isLeft ? mGrabRotateSpeed : -mGrabRotateSpeed;
+        mHeldAngle = Mathf.Clamp(mHeldAngle, -mGrabMaxRotation, mGrabMaxRotation);
+    }
+
+    /// hold the item in place
+    void HoldItem() {
+        // build rotation from hand position
+        var forward = mHand.forward;
+        var rotation = Quaternion.LookRotation(
+            forward,
+            Quaternion.AngleAxis(mHeldAngle, forward) * mHand.up
+        );
+
+        // move and rotate item towards hand
+        var ti = mHeld.transform;
+        ti.position = Vector3.Lerp(ti.position, mHand.position, 0.1f);
+        ti.rotation = Quaternion.Lerp(ti.rotation, rotation, 0.1f);
     }
 
     /// release the held item
     void ReleaseItem() {
-        // remove the item from the joint
-        mHand.connectedBody = null;
-        mHand.SetActive(false);
-    }
+        // reset the item
+        mHeld.isKinematic = false;
+        mHeld.gameObject.layer = sItemLayer;
 
-    // -- queries --
-    /// if the player is holding an item
-    bool IsHoldingItem {
-        get => mHand.IsActive();
+        // release it from the hand
+        mHeld = null;
+        mHeldAngle = 0.0f;
     }
 }
